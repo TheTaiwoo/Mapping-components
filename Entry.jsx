625e85db764d21a3c92813a08899698155e3c573
Taiwo Talabi <97107272+TheTaiwoo@users.noreply.github.com>
2024-04-26 22:30:39 -0700

    Add files via upload

---
+++ Entry.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+//This is the visual representation of what you are trying to build
+function Entry(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emoji}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <dd>{props.description}</dd>
+    </div>
+  );
+}
+
+export default Entry;
